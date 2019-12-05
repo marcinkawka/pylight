6ff300af48243c7110ccdfee1a2ed0e65d70fc6a
marcinkawka <kawka.marcin@gmail.com>
2019-12-05 21:48:48 +0100

    Added matplotlib examples

--- Przyklad_01_matplotlib.py
+++ Przyklad_01_matplotlib.py
@@ -2,27 +2,46 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 # wczytywanie danych z csv za pomocą pandas
-df=pd.read_csv('tasmax_count_gt_30_rcp45_Warszawa_trend.csv')
+df = pd.read_csv('dane_zsumowane_z_pogoda.csv')
+df['dzien']=pd.to_datetime(df['Data'])
+df.set_index(['dzien'],inplace=True)
 #wybór zmiennej do wykresu
-y = df['liczba_dni']
-
+kolumna1 = 'Marszałkowska/Metro Świętokrzyska'
+kolumna2 = 'Banacha/Żwirki i Wigury'
+kolumna3 = 'Belwederska'
+'''
 #proste rysowanie wykresu
-#plt.plot(y)
-#plt.show()
+plt.plot(df[[kolumna1,kolumna2]])
 
-#dodajmy zmienną niezależną
-x=range(2007,2093)
-plt.plot(x,y,'kx')
-#i kilka ozdobników
+#kilka ozdobników
+plt.legend([kolumna1,kolumna2])
+plt.title('Dzienna liczba rowerzystów')
 plt.grid(True)
-plt.xlabel('Rok')
-plt.ylabel('Liczba dni w roku')
-plt.title('Liczba dni w roku z temperaturą maksymalną powyżej 30 stopni')
-
 plt.show()
 
+'''
+#wykres rozproszenia
+#plt.scatter(x=df[kolumna1],y=df[kolumna2])
+#plt.show()
 
-#subploty
+'''
+#subploty (3,1, przełącz na 1), notacja Matlabowa
+plt.subplot(3,1,1)
+plt.plot(df[kolumna1])
+plt.subplot(3,1,2)
+plt.plot(df[kolumna2])
+plt.subplot(3,1,3)
+plt.plot(df[kolumna3])
+plt.show()
 
+#To samo, ale bardziej elegancko, notacja Pythonowa
+fig1, ax = plt.subplots(3,1,sharex=True)
+l1, = ax[0].plot(df[kolumna1],color='r')
+l2, = ax[1].plot(df[kolumna2],color='g')
+l3, = ax[2].plot(df[kolumna3],color='b')
+plt.legend([l1,l2,l3],[kolumna1,kolumna2,kolumna3])
+plt.grid(True)
+plt.show()
+'''
 # Zapis do pliku zamiast na ekran
 #plt.savefig('foo.pdf')
